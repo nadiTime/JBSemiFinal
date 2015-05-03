63ba14766b3dae7c508bd15d62e7deeff6cb41a8
nadav cohen <nadavcn@gmail.com>
2015-05-03 21:32:23 +0300

    ready to deploy friend details, did sever side varification to make sure not the same id

--- api/userDetails.php
+++ api/userDetails.php
@@ -1,51 +1,57 @@
 <?php
 session_start();
+	$status=false;
 	require_once ('functions.php');
 	if(isset($_GET['userId'])&&isset($_GET['friendId'])){
-		$user_id=$_GET['userId'];
-		$friend_id=$_GET['friendId'];
-		$sqlObj = connect();
-		$sql="SELECT `email` as user_email, nickname as user_nickname, birthdate as user_birthdate,
-		 about as user_about, image_path, register_date FROM `users` WHERE id=$friend_id";
-		//select reg data
-		$answer= $sqlObj->query($sql);
-		if($answer){
-			$array['regData'] =  mysqli_fetch_assoc($answer);	
-		}
-		//select posts
-		$sql="SELECT `post` as post, `date` as post_date FROM `posts`
-		 WHERE user_id=$friend_id ORDER BY post ASC";
-		$answer= $sqlObj->query($sql);
-		if($answer){
-			$arr = [];
-			while($res = mysqli_fetch_assoc($answer)){
-				$arr[] =$res;
-			}
-			$array['posts'] = $arr; 
-		}
+		if($_GET['userId']!=$_GET['friendId']){
 
-		//now check request status with friend
-		$sql = "SELECT `status`,`sender_id`,`reciever_id` FROM `requests`
-		 WHERE `reciever_id` = '$user_id' AND `sender_id` = '$friend_id'
-		 OR `sender_id` = '$user_id' AND `reciever_id` = '$friend_id'";
-		$answer= $sqlObj->query($sql);
-		if($answer){
-			$array['requests']  = mysqli_fetch_assoc($answer);
-		}
-
-		//check if status 1 and get secret data
-
-		if($array['requests']['status']==1){
-			$sql = "SELECT `secret_image_path` as user_secret_image,`note` as user_secret_note
-			 FROM `secret`
-			 WHERE `user_id` = '$friend_id'";
+			$user_id=$_GET['userId'];
+			$friend_id=$_GET['friendId'];
+			$sqlObj = connect();
+			$sql="SELECT `email` as user_email, nickname as user_nickname, birthdate as user_birthdate,
+			 about as user_about, image_path, register_date FROM `users` WHERE id=$friend_id";
+			//select reg data
 			$answer= $sqlObj->query($sql);
 			if($answer){
-				$array['secret']  = mysqli_fetch_assoc($answer);
+				$array['regData'] =  mysqli_fetch_assoc($answer);
+				$status = true;	
 			}
+			//select posts
+			$sql="SELECT `post` as post, `date` as post_date FROM `posts`
+			 WHERE user_id=$friend_id ORDER BY post ASC";
+			$answer= $sqlObj->query($sql);
+			if($answer){
+				$arr = [];
+				while($res = mysqli_fetch_assoc($answer)){
+					$arr[] =$res;
+				}
+				$array['posts'] = $arr; 
+			}
+
+			//now check request status with friend
+			$sql = "SELECT `status`,`sender_id`,`reciever_id` FROM `requests`
+			 WHERE `reciever_id` = '$user_id' AND `sender_id` = '$friend_id'
+			 OR `sender_id` = '$user_id' AND `reciever_id` = '$friend_id'";
+			$answer= $sqlObj->query($sql);
+			if($answer){
+				$array['requests']  = mysqli_fetch_assoc($answer);
+			}
+
+			//check if status 1 and get secret data
+
+			if($array['requests']['status']==1){
+				$sql = "SELECT `secret_image_path` as user_secret_image,`note` as user_secret_note
+				 FROM `secret`
+				 WHERE `user_id` = '$friend_id'";
+				$answer= $sqlObj->query($sql);
+				if($answer){
+					$array['secret']  = mysqli_fetch_assoc($answer);
+				}
+			}
+			
 		}
-		
 	}
+		
 	elseif (isset($_GET['userId'])) {
 		$user_id=$_GET['userId'];
 		$sqlObj = connect();
@@ -53,7 +59,8 @@ session_start();
 		about as user_about, image_path, register_date FROM `users` WHERE id=$user_id";
 		$answer= $sqlObj->query($sql);
 		if($answer){
-			$array['regData'] =  mysqli_fetch_assoc($answer);	
+			$array['regData'] =  mysqli_fetch_assoc($answer);
+			$status =true;	
 		}
 		$sql="SELECT `post` as post, `date` as post_date FROM `posts`
 		 WHERE user_id=$user_id ORDER BY post ASC";
@@ -74,4 +81,6 @@ session_start();
 			}
 
 	}
-	echo json_encode($array);	
+	if($status){
+		echo json_encode($array);	
+	}
--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 	var userId = sessionStorage.getItem('userId');
+	sessionStorage.setItem('lastPage',6);
 	if(sessionStorage.getItem('lastPage')){ //id if from friendships.php or friends.php 0 from else
 		var friendId=sessionStorage.getItem('lastPage');
 		$.getJSON("api/userDetails.php",{userId:userId,friendId:friendId})//ajax(friendId  and userId) to get friends details from userDetails.php
 		.done(function(data){
-
+			console.log(data);
 		})
 		
 		/*data returned
@@ -23,22 +24,22 @@ $(document).ready(function(){
 	/*take user id from sessionStorage and get user details from userDetails.php
 		name, birth, email, image, posts, secret image, secret data
 	*/
-	$.getJSON("api/userDetails.php",{userId:userId})
-	.done(function(data){
-		var tmp = data.regData;
-		$("#user_details").children().each(function(){
-			var key = $(this).attr("name");
-			this.innerHTML = tmp[key];
-		});
-		$("#profile-picture img").attr("src",tmp['image_path']);
-		$("#secret_data p").text(data.secret.user_secret_image); 
-		$("#secret_data img").attr("src",data.secret.user_secret_note);
-		for(var prop in data.posts){
-			var post = $("<p class='user_post'>"+data.posts[prop]['post']+"<span class='post_date'>"+data.posts[prop]['post_date']+"</span>");
-			$("#posts_view").append(post);
-		}
-		
-	})
+	else{$.getJSON("api/userDetails.php",{userId:userId})
+		.done(function(data){
+			var tmp = data.regData;
+			$("#user_details").children().each(function(){
+				var key = $(this).attr("name");
+				this.innerHTML = tmp[key];
+			});
+			$("#profile-picture img").attr("src",tmp['image_path']);
+			$("#secret_data p").text(data.secret.user_secret_image); 
+			$("#secret_data img").attr("src",data.secret.user_secret_note);
+			for(var prop in data.posts){
+				var post = $("<p class='user_post'>"+data.posts[prop]['post']+"<span class='post_date'>"+data.posts[prop]['post_date']+"</span>");
+				$("#posts_view").append(post);
+			}
+			
+		})}
 
 })
 
